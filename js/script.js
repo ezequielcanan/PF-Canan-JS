3bad82388c8be3cb176a21cd25bfe1c33a523139
Ezequiel Canan <ezequielcanan@gmail.com>
2023-07-01 12:10:16 -0300

    Pasar todo a local, avances

--- js/script.js
+++ js/script.js
@@ -1,34 +1,51 @@
-let pcsArr = [
-  { id: 1, gpu: "2060s", mother: "Gigabyte", cpu: "Intel I3-10100F", ram: "16", ssd: 480, price: 979, pathImage: `./multimedia/pc1.png`},
-  { id: 2, gpu: "3070ti", mother: "Asus", cpu: "Intel I7-10700F", ram: "16", ssd: 480, price: 1189, pathImage: `./multimedia/pc2.png`},
-  { id: 3, gpu: "3090ti", mother: "Aorus", cpu: "Intel I9-13900K", ram: "32", ssd: 480, price: 1239, pathImage: `./multimedia/pc3.png`},
-  { id: 4, gpu: "4090ti", mother: "Asus", cpu: "AMD 5600G", ram: "32", ssd: 1000, price: 1489, pathImage: `./multimedia/pc4.jpg`},
-];
-let appliedFilters = {}
-let carrito = []
+function main() {
+  let pcsArr = [
+    { id: 1, gpu: "2060s", mother: "Gigabyte", cpu: "Intel", cpuDescription: "Intel I3-10100F", ram: "16gb", ssd: "480gb", price: 979, pathImage: `./multimedia/pc1.png` },
+    { id: 2, gpu: "3070ti", mother: "Asus", cpu: "Intel", cpuDescription: "Intel I7-10700F", ram: "16gb", ssd: "480gb", price: 1189, pathImage: `./multimedia/pc2.png` },
+    { id: 3, gpu: "3090ti", mother: "Aorus", cpu: "Intel", cpuDescription: "Intel I9-13900K", ram: "32gb", ssd: "480gb", price: 1239, pathImage: `./multimedia/pc3.png` },
+    { id: 4, gpu: "4090ti", mother: "Asus", cpu: "AMD", cpuDescription: "AMD 5600G", ram: "32gb", ssd: "1000gb", price: 1489, pathImage: `./multimedia/pc4.jpg` },
+  ];
+  createFilters(pcsArr)
+  renderizar(pcsArr)
+  /*Boton reset filtros*/
+  let inputsFiltros = document.getElementsByClassName("input-filtro")
+  let resetFilters = document.getElementById("resetFilters")
+  resetFilters.onclick = () => {
+    resetearFiltros(inputsFiltros, pcsArr)
+  }
+  /*Evento agregar al carrito*/
+  let buttonsAgregarAlCarrito = document.querySelectorAll(".btn-buy-card")
+  for (const btn of buttonsAgregarAlCarrito) {
+    btn.addEventListener("click", ({ target: { id } }) => agregarAlCarrito(id))
+  }
 
-function renderizar() {
+}
+
+main()
+//let appliedFilters = {}
+//let carrito = []
+
+function renderizar(arr) {
   let cardsContainer = document.getElementById("cardsContainer");
   cardsContainer.innerHTML = ""
-  let pcsRender = pcsArr
-  for (let filtro in appliedFilters) {
-    pcsRender = pcsRender.filter((pc) => pc[filtro].toLowerCase().includes(appliedFilters[filtro]))
+  let pcsRender = arr
+  for (let filtro in filtrosAObjeto()) {
+    pcsRender = pcsRender.filter((pc) => pc[filtro].toLowerCase().includes(filtrosAObjeto()[filtro].toLowerCase()))
   }
   pcsRender.forEach((card) => {
     cardsContainer.innerHTML += `<div class="card container__tienda__items__cards__card mb-3 col-md-5">
     <img src=${card.pathImage} class="card-img-top" alt="Imagen de pc con la gpu ${card.gpu}">
     <div class="card-body container__tienda__items__cards__card__body">
     <h4 class="card-title">${card.price} USD</h4>
-    <p class="card-text">PC armada gamer con ${card.ram}gb de ram, ${card.ssd}gb, ${card.cpu} y la gpu RTX ${card.gpu}.</p>
+    <p class="card-text">PC armada gamer con ${card.ram} de ram, ${card.ssd}gb, ${card.cpuDescription} y la gpu RTX ${card.gpu}.</p>
     <button class="btn btn-buy-card" id=${card.id}>Agregar al carrito</button>
     </div>
     </div>`;
   });
 }
-renderizar()
 
-function agregarAlCarrito(id) {
-  let producto = pcsArr.find((pc) => pc.id == id)
+function agregarAlCarrito(id, arr) {
+  let producto = arr.find((pc) => pc.id == id)
   if (carrito.includes(producto)) {
     carrito[carrito.indexOf(producto)].cantidad++
   } else {
@@ -37,23 +54,44 @@ function agregarAlCarrito(id) {
   }
 }
 
-/*Evento filtros*/
-let filtros = document.querySelectorAll(".input-filtro")
-filtros.forEach((filtro) => {
-  filtro.addEventListener("input", (e) => {
-    appliedFilters[e.target.name] = e.target.id
-    renderizar()
+function createFilters(arr) {
+  let filtros = ["cpu", "mother", "ram", "ssd"]
+  filtros.forEach((filtro) => {
+    let ulFiltro = document.getElementsByClassName(`container__tienda__aside__filtro-${filtro}`)[0].getElementsByClassName("container__tienda__aside__filtro__ul")
+    let options = []
+    arr.forEach((pc) => {
+      if (!options.includes(pc[filtro])) {
+        let option = document.createElement("li")
+        option.classList.add("container-input")
+        option.innerHTML = `<input class="input-filtro" type="radio" name="${filtro}" value="${pc[filtro]}" id="${pc[filtro]}">
+        <label class="label-filtro" for="${pc[filtro]}">${pc[filtro]}</label>`
+        options.push(pc[filtro])
+        ulFiltro[0].appendChild(option)
+
+        let inputBtn = document.getElementById(pc[filtro])
+        inputBtn.addEventListener("input", ({target:{id, name}}) => applyFilter(id, name, arr))
+      }
+    })
   })
-})
-/*Boton reset filtros*/
-let resetFilters = document.getElementById("resetFilters")
-resetFilters.onclick = () => {
-  appliedFilters = {}
-  filtros.forEach((f) => f.checked = false)
-  renderizar()
+  sessionStorage.setItem("filtros", JSON.stringify({}))
 }
-/*Evento agregar al carrito*/
-let buttonsAgregarAlCarrito = document.querySelectorAll(".btn-buy-card")
-buttonsAgregarAlCarrito.forEach((btn) => {
-  btn.addEventListener("click", ({target: {id}}) => agregarAlCarrito(id))
-})
+
+function applyFilter(filtro, name, arr) {
+  let jsonFiltros = sessionStorage.getItem("filtros")
+  let objectFiltros = JSON.parse(jsonFiltros)
+  objectFiltros[name] = filtro
+  sessionStorage.setItem("filtros", JSON.stringify(objectFiltros))
+  renderizar(arr, filtrosAObjeto())
+}
+
+function filtrosAObjeto() {
+  return JSON.parse(sessionStorage.getItem("filtros"))
+}
+
+function resetearFiltros(arr1,arr2) {
+  for (const filtro of arr1) {
+    filtro.checked = false
+  }
+  sessionStorage.setItem("filtros", JSON.stringify({}))
+  renderizar(arr2)
+}
